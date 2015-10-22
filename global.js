e32a0c3fc233c178bb19047394eb6996dfa2e74a
Robin <robinledru@eduroam-1-12.enst.fr>
2015-10-22 16:53:26 +0200

    Code du 22/10

---
+++ global.js
@@ -0,0 +1,24 @@
+// Custom JS
+$(document).ready(function(){
+  // Add your code here
+  // ------------------------------
+
+	function scrollNav() {
+	  $('.nav a').click(function(){  
+		//Toggle Class
+		$(".active").removeClass("active");      
+		$(this).closest('li').addClass("active");
+		var theClass = $(this).attr("class");
+		$('.'+theClass).parent('li').addClass('active');
+		//Animate
+		$('html, body').stop().animate({
+			scrollTop: $( $(this).attr('href') ).offset().top - 40
+		}, 200);
+		return false;
+	  });
+	  $('.scrollTop a').scrollTop();
+	}
+
+
+	scrollNav();
+});
